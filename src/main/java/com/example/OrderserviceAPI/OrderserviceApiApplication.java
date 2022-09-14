8639ef30ad4e07dcf40effc7f4fe391863895230
Akshayam <akshaym30.ap@gmail.com>
2022-09-14 16:16:45 +0530

    Second time

--- src/main/java/com/example/OrderserviceAPI/OrderserviceApiApplication.java
+++ src/main/java/com/example/OrderserviceAPI/OrderserviceApiApplication.java
@@ -8,7 +8,7 @@ public class OrderserviceApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderserviceApiApplication.class, args);
-		System.out.println("Application Started");
+		System.out.println("Application Begin");
 	}
 
 }
